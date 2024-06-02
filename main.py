bc5d219af2df343ac639817edfa84fb26dc2429e
Lasse-van-iterson <lassevaniterson@live.nl>
2024-06-02 19:55:10 +0200

    Added print sprite to print the requested pokemon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from PIL import Image
+from io import BytesIO
 
 def main():
     print("Hello, welcome to the python pokedex!")
@@ -23,6 +25,38 @@ def print_pokemon(pokemon):
     print(f"Types: ")
     for type in pokemon['types']:
         print(f"  - {type['type']['name']}")
+    print_sprite(pokemon['sprites']['front_default'])
+    print(f"Sprite: {pokemon['sprites']['front_default']}")
+
+def rgb_to_ansi(r, g, b):
+    return f"\033[48;2;{r};{g};{b}m  \033[0m"
+
+def print_sprite(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        img = Image.open(BytesIO(response.content))
+        img = img.crop((15, 15, 90, 90)) 
+        img = img.resize((40, 40))  
+
+        
+        if img.mode in ('RGBA', 'LA') or (img.mode == 'P' and 'transparency' in img.info):
+            img = img.convert('RGBA')
+        else:
+            img = img.convert('RGB')
+        
+        for y in range(img.height):
+            for x in range(img.width):
+                pixel = img.getpixel((x, y))
+                if len(pixel) == 4:
+                    r, g, b, a = pixel
+                    if a > 0:
+                        print(rgb_to_ansi(r, g, b), end='')
+                    else:
+                        print('  ', end='')
+                else: 
+                    r, g, b = pixel
+                    print(rgb_to_ansi(r, g, b), end='')
+            print('')
 
 if __name__ == "__main__":
     main()
